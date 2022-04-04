6038e278d6d09d3f372f54599fd389bb0e287672
Zaid Bin Shahab <ZaidBShahab@gmail.com>
2022-04-04 14:58:20 +0500

    Server will also be able to send message

--- client.py
+++ client.py
@@ -1,37 +1,36 @@
-from email import message
-from pydoc import cli
 import socket
 import threading
 
+#Your Username
 username = input("Enter username : ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
+client.connect(('127.0.0.1', 55555))    #connect to the server via it's IP address and port
 
-def receive():
+def receive():   #utility function that recieves messages from the server
+    
     while True:
         try:
             message = client.recv(1024).decode('ascii')
 
-            if message == "u_name" :
+            if message == "u_name" :    # A silent message from server asking it's username
                 client.send(username.encode('ascii'))
             else :
                 print(message)
         
-        except:
-            print("Error 69")
+        except:     #in case the server closes
+            print("Error 69 : Server Time Out")
             client.close()
             break
 
-def write():
+def write():    #utility function to write a new message
     while True:
         text = input("")
         messageFormat = "{} : {}".format(username, text)
         client.send(messageFormat.encode('ascii'))
 
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
-
-write_thread = threading.Thread(target=write)
-write_thread.start()
+receiver = threading.Thread(target=receive)
+receiver.start()
+writer = threading.Thread(target=write)
+writer.start()
--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-# from http import server
 import threading
 import socket
 
@@ -7,15 +6,15 @@ port = 55555
 clients = []    #client IP address
 usernames = []  #client username
 
-def broadcast(message):
+def broadcast(message):     #Utility to broadcast message to all clients
     for client in clients:
         client.send(message)
 
-def send(client):
-
+def manage(client):     #recieving every messages and broadcasting, incase user leaves, so broadcast
+                        #and close that client connection.
     while True:
         try:
-            message = client.recv(1024)
+            message = client.recv(50)   #only 50 peoples are allowed in chatroom. Tentative!
             broadcast(message)
         except:
             client_num = clients.index(client)
@@ -27,21 +26,34 @@ def send(client):
             client.close()
             break
 
-def recieve():
+def write():       #Utility function to write any message from Server
+    
+    while True:
+        text = input("")
+        messageFormat = "Server : {}".format(text)
+        broadcast(messageFormat.encode("ascii"))
+
+def recieve():  #Kinda main func that will accept the client to chatroom
 
     while True:
         client, address = server.accept()
+        
         #asks for the client username
-        client.send('u_name'.encode('ascii'))
-        u_name = client.recv(1024).decode('ascii')
+        client.send('u_name'.encode('ascii'))   #'u_name' is a secret message to client asking its username
+        u_name = client.recv(50).decode('ascii')
         usernames.append(u_name)
         clients.append(client)
 
+        print("{} : {} has joined the server".format(address, u_name))
         broadcast("{} has joined the chat".format(u_name).encode('ascii'))
         client.send("You have successfully joined the chat group".encode('ascii'))
 
-        thread = threading.Thread(target=send, args=(client,))
-        thread.start()
+        #Two thread running simulataneously for manage and write functions
+        manager = threading.Thread(target=manage, args=(client,))
+        manager.start()
+        writer = threading.Thread(target=write)
+        writer.start()
+
 
 
 #AF_INET : using an internet socket
@@ -49,5 +61,4 @@ def recieve():
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((hostIP, port))
 server.listen()
-
 recieve()
